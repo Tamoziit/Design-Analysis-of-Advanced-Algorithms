fc3cf41923f3887df21234796566f6846a9f53af
Tamoziit <tamojitdas181007@gmail.com>
2025-03-29 11:33:33 +0530

    Dijkstra update: 2

--- graph_algorithms/dijkstra.c
+++ graph_algorithms/dijkstra.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 100
+
+typedef struct edge
+{
+    int dest;
+    int weight;
+} Edge;
+
+typedef struct adjlist
+{
+    int deg;
+    Edge *arr;
+} AdjList;
 
 typedef struct vertex
 {
@@ -8,18 +21,11 @@ typedef struct vertex
     int idx;
 } Node;
 
-typedef struct adjArr
-{
-    int degree;
-    Node *vertex;
-} AdjArr;
-
 typedef struct graph
 {
     int numVertices;
     Node *vertices;
-    AdjArr *adjList;
-    int **weights;
+    AdjList *adjList;
 } Graph;
 
 void minHeapify(Node *arr, int i, int size)
@@ -89,8 +95,14 @@ Node dequeue(Node *arr, int size, int *i)
     return min;
 }
 
-void heapDecreaseKey(Node *arr, int i, int newKey, int heapSize)
+void heapDecreaseKey(Node *arr, int i, int newKey, int heapSize, int idx)
 {
+    if (idx < 0)
+    {
+        printf("Underflow\n");
+        return;
+    }
+
     if (i < 0 || i > heapSize - 1)
     {
         printf("Invalid Index\n");
@@ -119,6 +131,16 @@ void heapDecreaseKey(Node *arr, int i, int newKey, int heapSize)
     }
 }
 
+void printQueue(Node *queue, int idx)
+{
+    int i;
+    for (i = 0; i < idx; i++)
+    {
+        printf("(%d | %d) ", queue[i].idx, queue[i].key);
+    }
+    printf("\n");
+}
+
 int isInQueue(Node *arr, int size, int idx)
 {
     int i;
@@ -141,54 +163,36 @@ Graph *createGraph(int numVertices)
     }
 
     graph->numVertices = numVertices;
-    graph->adjList = (AdjArr *)malloc(numVertices * sizeof(AdjArr));
+    graph->adjList = (AdjList *)malloc(numVertices * sizeof(AdjList));
     graph->vertices = (Node *)malloc(numVertices * sizeof(Node));
-    graph->weights = (int **)malloc(numVertices * sizeof(int *));
 
-    if (!graph->adjList || !graph->vertices || !graph->weights)
+    if (!graph->adjList || !graph->vertices)
     {
         printf("Memory allocation failed for graph components\n");
         exit(1);
     }
 
-    for (int i = 0; i < numVertices; i++)
+    int i;
+    for (i = 0; i < numVertices; i++)
     {
-        graph->weights[i] = (int *)malloc(numVertices * sizeof(int));
-        if (!graph->weights[i])
-        {
-            printf("Memory allocation failed for weights\n");
-            exit(1);
-        }
+        graph->adjList[i].arr = (Edge *)malloc(numVertices * sizeof(Edge));
     }
 
     // Graph initialization
-    for (int i = 0; i < numVertices; i++)
+    for (i = 0; i < numVertices; i++)
     {
         graph->vertices[i].key = 9999;
         graph->vertices[i].parent = -1;
         graph->vertices[i].idx = i;
-        graph->adjList[i].degree = 0;
-        graph->adjList[i].vertex = (Node *)malloc(numVertices * sizeof(Node));
-        if (!graph->adjList[i].vertex)
-        {
-            printf("Memory allocation failed for adjacency list\n");
-            exit(1);
-        }
-
-        for (int j = 0; j < numVertices; j++)
-        {
-            graph->weights[i][j] = 0;
-            graph->adjList[i].vertex[j].key = 9999;
-            graph->adjList[i].vertex[j].parent = -1;
-        }
+        graph->adjList[i].deg = 0;
     }
 
     return graph;
 }
 
-void readGraphFromFile(Graph *graph)
+void readGraphFromFile(Graph *graph, char *fileName)
 {
-    FILE *file = fopen("graph.txt", "r");
+    FILE *file = fopen(fileName, "r");
     if (!file)
     {
         printf("Error opening input file!\n");
@@ -204,7 +208,7 @@ void readGraphFromFile(Graph *graph)
             continue;
         }
 
-        graph->adjList[vertex].degree = degree;
+        graph->adjList[vertex].deg = degree;
         for (i = 0; i < degree; i++)
         {
             fscanf(file, "%d %d", &neighbor, &weight);
@@ -215,82 +219,139 @@ void readGraphFromFile(Graph *graph)
                 continue;
             }
 
-            graph->weights[vertex][neighbor] = weight;
+            graph->adjList[vertex].arr[i].dest = neighbor;
+            graph->adjList[vertex].arr[i].weight = weight;
         }
     }
 
     fclose(file);
 }
 
-void prims(Graph *graph, int s, Node *queue, int *idx)
+void relax(Graph *graph, int u, int v, Node *queue, int idx)
+{
+    int wt, i, j = 0;
+    for (i = 0; i < graph->adjList[u].deg; i++)
+    {
+        if (graph->adjList[u].arr[i].dest == v)
+            wt = graph->adjList[u].arr[i].weight;
+    }
+
+    for (i = 0; i < graph->numVertices; i++)
+    {
+        if (v == queue[i].idx)
+            j = i;
+    }
+
+    if (graph->vertices[v].key > graph->vertices[u].key + wt)
+    {
+        graph->vertices[v].key = graph->vertices[u].key + wt;
+        graph->vertices[v].parent = u;
+        heapDecreaseKey(queue, j, graph->vertices[u].key + wt, graph->numVertices, idx);
+    }
+}
+
+void dijkstra(Graph *graph, Node *queue, int *idx, int s)
 {
     graph->vertices[s].key = 0;
-    heapDecreaseKey(queue, s, 0, graph->numVertices);
-    int i, j;
+    int i, j, v;
     Node u;
     for (i = 0; i < graph->numVertices; i++)
     {
         enqueue(queue, graph->numVertices, idx, graph->vertices[i]);
     }
 
+    printf("Initally the queue:\n");
+    printQueue(queue, (*idx));
+
     while ((*idx) != -1) // Empty
     {
         u = dequeue(queue, graph->numVertices, idx);
-        for (i = 0; i < graph->numVertices; i++)
+        for (j = 0; j < graph->adjList[u.idx].deg; j++)
         {
-            if (graph->weights[u.idx][i] != 0)
-            {
-                if (isInQueue(queue, graph->numVertices, i) && graph->weights[u.idx][i] < graph->vertices[i].key)
-                {
-                    graph->vertices[i].parent = u.idx;
-                    graph->adjList[u.idx].vertex[i].parent = u.idx;
-                    graph->vertices[i].key = graph->weights[u.idx][i];
-                    graph->adjList[u.idx].vertex[i].key = graph->weights[u.idx][i];
-                    heapDecreaseKey(queue, i, graph->weights[u.idx][i], graph->numVertices);
-                }
-            }
+            v = graph->adjList[u.idx].arr[j].dest;
+            relax(graph, u.idx, v, queue, (*idx));
+            printQueue(queue, (*idx));
         }
     }
 }
 
 void printGraph(Graph *graph)
 {
-    int v, j, w, p;
+    int v, j, wt, p, key, dest;
+    printf("Vertex\t\t(Destination | Weight | Key | Parent)\n");
     for (v = 0; v < graph->numVertices; v++)
     {
-        printf("%d: ", v);
-        for (j = 0; j < graph->numVertices; j++)
+        printf("%d:\t", v);
+        for (j = 0; j < graph->adjList[v].deg; j++)
         {
-            w = graph->adjList[v].vertex[j].key;
-            p = graph->adjList[v].vertex[j].parent;
-            if (graph->weights[v][j] != 0)
-            {
-                printf("%d/%d/%d ", j, p, w);
-            }
+            dest = graph->adjList[v].arr[j].dest;
+            wt = graph->adjList[v].arr[j].weight;
+            key = graph->vertices[v].key;
+            p = graph->vertices[v].parent;
+
+            printf("(%d | %d | %d | %d)\t", dest, wt, key, p);
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void printMST(Graph *graph)
+void printDistance(Graph *graph)
 {
     int v;
-    printf("Vertex\t   Predecessor\t\tKey\n");
-    for(v=0; v<graph->numVertices; v++)
+    printf("Vertex\tDistance\n");
+    for (v = 0; v < graph->numVertices; v++)
     {
-        printf("%d\t\t%d\t\t%d\n", graph->vertices[v].idx, graph->vertices[v].parent, graph->vertices[v].key);
+        printf("%d\t\t%d\n", v, graph->vertices[v].key);
     }
     printf("\n");
+}
+
+int printPath(Graph *graph, int src, int dest)
+{
+    if (src == dest)
+    {
+        printf("%d ", dest);
+        return 0;
+    }
+
+    if (graph->vertices[dest].parent == -1)
+    {
+        printf("No path exists.\n");
+        return -1;
+    }
+
+    int parent = graph->vertices[dest].parent;
+    int weight = 0, i;
+
+    for (i = 0; i < graph->adjList[parent].deg; i++)
+    {
+        if (graph->adjList[parent].arr[i].dest == dest)
+        {
+            weight = graph->adjList[parent].arr[i].weight;
+            break;
+        }
+    }
+
+    int totalCost = printPath(graph, src, parent);
+    if (totalCost == -1)
+        return -1;
+
+    printf("-> %d ", dest);
+    return totalCost + weight;
 }
 
 int main()
 {
     int n, i = -1, s;
+    char *file = (char *)malloc(MAX * sizeof(char));
+
+    printf("Enter Input File Name\n");
+    scanf("%s", file);
     printf("Enter no. of vertices\n");
     scanf("%d", &n);
     Graph *graph = createGraph(n);
-    readGraphFromFile(graph);
+    readGraphFromFile(graph, file);
     Node *queue = (Node *)malloc(n * sizeof(Node));
 
     printf("\nThe Graph\n");
@@ -298,10 +359,18 @@ int main()
 
     printf("Enter source index\n");
     scanf("%d", &s);
-    prims(graph, s, queue, &i);
-    printf("After Prims -> MST\n");
-    printMST(graph);
+    dijkstra(graph, queue, &i, s);
+    printf("\nAfter Dijkstra -> Distance from Src. vertex %d:\n", s);
+    printDistance(graph);
 
+    int src, dest;
+    printf("Enter source & destination\n");
+    scanf("%d%d", &src, &dest);
+    int cost = printPath(graph, src, dest);
+    printf(" : Path Cost = %d\n", cost);
+
+    free(queue);
+    free(file);
     free(graph);
     return 0;
 }
